63e2b102783517c98f15927e6127774a1e93c188
Bombearo <bombearogames@gmail.com>
2021-05-03 15:27:41 +0100

    Basic API connection

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Pokemon/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Pokemon from './components/Pokemon.vue'
+
+const Pokedex = require("pokeapi-js-wrapper")
+const P = new Pokedex.Pokedex()
+const interval = {
+  offset: 34,
+  limit: 10,
+}
+P.getPokemonsList(interval).then(function(response) {
+  console.log(response)
+})
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Pokemon
   }
 }
 </script>
