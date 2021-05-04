f13d3b1800f9cb167acf9e8a413a94a5d6965510
Bombearo <bombearogames@gmail.com>
2021-05-04 21:56:34 +0100

    Initialised Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <PokemonList :start="0" :end="60" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import PokemonList from './components/PokemonList.vue'
-
-
-export default {
-  name: 'App',
-  components: {
-    PokemonList
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,7 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  margin: auto;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
