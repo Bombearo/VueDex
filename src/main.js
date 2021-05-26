660c3dc76f28492d64d0de1804d46de03f12aec6
bombearo <shekjaden@gmail.com>
2021-05-26 15:00:37 +0100

    Obtained a list of game versions

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 
 const app = createApp(App)
 const Pokedex = require("pokeapi-js-wrapper")
+
+
 const P = new Pokedex.Pokedex()
 app.config.globalProperties.dex = P
 app.use(router).mount('#app')
--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <h1>{{$route.params.name}}</h1>
-    <PokemonDetail :id ="$route.params.name" :name ="$route.params.name" />
+    <PokemonDetail :id ="$route.params.name" :name ="$route.params.name" class="condensedDetails"/>
+    <Generation :name ="$route.params.name"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import PokemonDetail from '@/components/PokemonDetail.vue'
+import Generation from '@/components/Generation.vue'
 
 export default {
   name: 'PokemonDetails',
   components:{
-    PokemonDetail
+    PokemonDetail,
+    Generation
   }
 }
 </script>
+
+<style scoped>
+.home{
+  width:80vw;
+  margin: auto;
+}
+.condensedDetails{
+  float:left
+}
+
+
+</style>
